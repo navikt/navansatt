0a16b0a0a548eaf6773f5581d82c81137e982b38
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-12-28 19:17:26 +0100

    Add more failure logging

--- src/main/kotlin/no/nav/navansatt/OidcDiscovery.kt
+++ src/main/kotlin/no/nav/navansatt/OidcDiscovery.kt
@@ -10,12 +10,16 @@ import io.ktor.client.request.url
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import org.apache.http.ssl.SSLContexts
+import org.slf4j.LoggerFactory
 
 @Serializable
 data class OIDCMetadata(
     val issuer: String,
     val jwks_uri: String
 )
+
+private val LOG = LoggerFactory.getLogger(OIDCMetadata::class.java)
+
 suspend fun oidcDiscovery(wellknownUrl: String): OIDCMetadata {
     val httpClient = HttpClient(Apache) {
         engine {
@@ -32,8 +36,13 @@ suspend fun oidcDiscovery(wellknownUrl: String): OIDCMetadata {
         }
     }
 
-    val meta = httpClient.get<OIDCMetadata> {
-        url(wellknownUrl)
+    try {
+        val meta = httpClient.get<OIDCMetadata> {
+            url(wellknownUrl)
+        }
+        return meta
+    } catch (err: Exception) {
+        LOG.error("Could not discover OIDC metadata at $wellknownUrl", err)
+        throw err
     }
-    return meta
 }
