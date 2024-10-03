c1976f863f6d0882e624947e4c4d9638020595e6
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-10-03 14:06:39 +0200

    Migrated to ktor 2, kotlin 2, JDK 21

--- src/test/kotlin/no/nav/navansatt/MockMain.kt
+++ src/test/kotlin/no/nav/navansatt/MockMain.kt
@@ -1,9 +1,11 @@
 package no.nav.navansatt.mock
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.concurrent.thread
 
-val LOG = LoggerFactory.getLogger("no.nav.navansatt.mock.MockMain")
+val LOG: Logger = LoggerFactory.getLogger("no.nav.navansatt.mock.MockMain")
+
 fun main() {
     LOG.info("Running mock backends")
     thread {
