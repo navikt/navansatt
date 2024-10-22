a58cc75c9b1e0f0221ddbc501a8d18a8adbbf503
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-10-22 13:55:31 +0200

    Endret feilhåndtering httpclient

--- src/main/kotlin/no/nav/navansatt/AxsysClient.kt
+++ src/main/kotlin/no/nav/navansatt/AxsysClient.kt
@@ -2,12 +2,13 @@ package no.nav.navansatt
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.plugins.ResponseException
+import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.url
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.isSuccess
 import kotlinx.serialization.Serializable
 import org.slf4j.LoggerFactory
 
@@ -43,34 +44,36 @@ class AxsysClient(val httpClient: HttpClient, val axsysUrl: String) {
     }
 
     suspend fun hentTilganger(ident: String): TilgangResponse {
-        try {
-            return httpClient.get {
-                url("$axsysUrl/api/v1/tilgang/$ident?inkluderAlleEnheter=false")
-                axsysHeaders()
-            }.body()
-        } catch (e: ResponseException) {
-            if (e.response.status == HttpStatusCode.NotFound) {
-                throw NAVAnsattNotFoundError("Fant ikke NAV-ansatt med id $ident")
-            } else {
-                LOG.error("Kunne ikke hente tilganger for NAV-ansatt $ident", e)
-                throw e
-            }
+        val httpResponse = httpClient.get {
+            url("$axsysUrl/api/v1/tilgang/$ident?inkluderAlleEnheter=false")
+            axsysHeaders()
+        }
+
+        if (httpResponse.status.isSuccess()) {
+            return httpResponse.body()
+        } else if (httpResponse.status == HttpStatusCode.NotFound) {
+            throw NAVAnsattNotFoundError("Fant ikke NAV-ansatt med id $ident")
+        } else {
+            val clientRequestException = ClientRequestException(httpResponse, "Kunne ikke hente tilganger for NAV-ansatt $ident")
+            LOG.error("Kunne ikke hente tilganger for NAV-ansatt $ident", clientRequestException)
+            throw clientRequestException
         }
     }
 
     suspend fun hentAnsattIdenter(enhetId: String): List<Ident> {
-        try {
-            return httpClient.get {
-                url("$axsysUrl/api/v1/enhet/$enhetId/brukere")
-                axsysHeaders()
-            }.body()
-        } catch (e: ResponseException) {
-            if (e.response.status == HttpStatusCode.NotFound) {
-                throw EnhetNotFoundError("Fant ikke NAV-enhet med id $enhetId")
-            } else {
-                LOG.error("Kunne ikke hente identer for NAV-enhet $enhetId", e)
-                throw e
-            }
+        val httpResponse = httpClient.get {
+            url("$axsysUrl/api/v1/enhet/$enhetId/brukere")
+            axsysHeaders()
+        }
+
+        if (httpResponse.status.isSuccess()) {
+            return httpResponse.body()
+        } else if (httpResponse.status == HttpStatusCode.NotFound) {
+            throw EnhetNotFoundError("Fant ikke NAV-enhet med id $enhetId")
+        } else {
+            val exception = ClientRequestException(httpResponse, "Kunne ikke hente identer for NAV-enhet $enhetId")
+            LOG.error("Kunne ikke hente identer for NAV-enhet $enhetId", exception)
+            throw exception
         }
     }
 }
